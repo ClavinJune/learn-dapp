202101f2896364f99f58ceaf5816fda18036d75b
Clavin June <juneardoc@gmail.com>
2021-11-27 23:24:20 +0700

    add comment
    Signed-off-by: Clavin June <juneardoc@gmail.com>

--- deploy.js
+++ deploy.js
@@ -1,13 +1,15 @@
 import { web3, account, networkGas } from "./network.js"
-import { bin, Interactor } from "./contract.js"
+import { bin } from "./contract.js"
 import { writeFileSync } from "fs"
 
 (async () => {
+    // deploying contract to local network
     const contract = await web3.eth.sendTransaction({
         from: account.address,
         gas: networkGas,
         data: bin,
     })
 
+    // write the result to be fetched later
     writeFileSync(`gen/contract.json`, JSON.stringify(contract))
 })()
